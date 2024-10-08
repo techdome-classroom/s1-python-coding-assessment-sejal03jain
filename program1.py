86c50818e2e3f6ae97192c3f3966dbab8bc8b21d
sejal jain <sejaljain032003@gmail.com>
2024-10-08 12:45:18 +0530

    Done

--- program1.py
+++ program1.py
@@ -1,6 +1,35 @@
 class Solution:
-   
     def getTotalIsles(self, grid: list[list[str]]) -> int:
-    #    write your code here
+        if not grid or len(grid) == 0:
+            return 0
+        
+        rows, cols = len(grid), len(grid[0])
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
+        
+        def dfs(r, c):
+        
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 'W' or visited[r][c]:
+                return
+            
+            
+            visited[r][c] = True
+            
+            
+            dfs(r - 1, c) 
+            dfs(r + 1, c) 
+            dfs(r, c - 1)  
+            dfs(r, c + 1)  
+        
+        island_count = 0
+        
+    
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 'L' and not visited[r][c]:
+            
+                    dfs(r, c)
                     
-        return 0
+                    
+                    island_count += 1
+                    
+        return island_count
