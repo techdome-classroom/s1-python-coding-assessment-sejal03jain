3b68da22ca4134f9923843a56a680036988c1534
sejal jain <sejaljain032003@gmail.com>
2024-10-08 12:54:10 +0530

    Done

--- program2.py
+++ program2.py
@@ -1,5 +1,24 @@
-def decode_message( s: str, p: str) -> bool:
+def decode_message(s: str, p: str) -> bool:
+    m, n = len(s), len(p)
+    
+    dp = [[False] * (m + 1) for _ in range(n + 1)]
+    
+    dp[0][0] = True
+    
 
-# write your code here
-  
-        return False
+    for i in range(1, n + 1):
+        if p[i - 1] == '*':
+            dp[i][0] = dp[i - 1][0]  
+    
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if p[i - 1] == s[j - 1] or p[i - 1] == '?':
+                
+                dp[i][j] = dp[i - 1][j - 1]
+            elif p[i - 1] == '*':
+        
+                dp[i][j] = dp[i - 1][j] or dp[i][j - 1]
+    
+
+    return dp[n][m]
